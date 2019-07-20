e52223d263f30705aef824dd3bfe6172c7be27c1
Kate Yanko <kateryanko@gmail.com>
2019-07-20 11:08:44 -0400

    added updated readme and webscraper.py with agent name, website, e-mail, and website

---
+++ README.md
@@ -0,0 +1,9 @@
+WEBSCRAPER PROJECT
+
+The goal of this project is to scrape the data from AgentQuery.com's list of Literary Agents. In their list of Literary Agents, they have the first and last name of the agent, their website (if applicable), the genres they represent, and the E-mail they prefer writers to send submissions to. 
+
+Names, agencies, and E-mail addresses were easy to scrape, but due to the HTML formatting of the webpage, it has proven quite challenging to break out the genres by agent. 
+
+Phase #2 of the project is to scrape the genres and save them in a meaningful way for re-use. 
+
+Once that is done, I can scrape all the data from the site and re-use it for a personal web application where I can search by genre and retrieve a list of relevant literary agents and their contact information. 
--- webscraper.py
+++ webscraper.py
@@ -8,29 +8,20 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 paragraphs=soup.findAll('p')
 for info_cards in paragraphs:
-	# name=info_cards.findAll(class_='result')
-	# email=info_cards.findAll('span', id=re.compile('Email'))
-#THIS IS WORKING, but we have to figure out how to print the text found after it. 
-	genres=info_cards.findAll(text=re.findall('FICTION GENRES',str(paragraphs)))
+	name=info_cards.findAll(class_='result')
+	email=info_cards.findAll('span', id=re.compile('Email'))
+# #THIS IS WORKING, but we have to figure out how to print the text found after it. 
+# 	genres=info_cards.findAll(text=re.findall('FICTION GENRES',str(paragraphs)))
 	# for titles in genres.find_next_sibling(text=True):
 	# 	print titles.string
 
-
-
-
-
-	# websites=info_cards.findAll('a', href=True, id=re.compile('lnkAgency'))
-	# for i in name:
-	# 	print i.text
-	# for j in email:
-	# 	print j.text
+	websites=info_cards.findAll('a', href=True, id=re.compile('lnkAgency'))
+	for i in name:
+		print i.text
+	for j in email:
+		print j.text
 	# for k in genres:
 	# 	print k
-	# for l in websites:
-	# 	print(l.get('href'))
+	for l in websites:
+		print(l.get('href'))
 	
-
-# genres=info_cards.findAll(text=True)
-#still need to get genres and agent websites
-#clean this up and make getnameandemail functions etc
-#read up on creating an array of dictionaries
